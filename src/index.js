f51d213e9580bc76eab3c733ecc3ad9bbcb77cc8
Svetlana Panenko <svetabagira1986@gmail.com>
2023-06-21 21:33:48 +0300

    fixeb basename

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/user">
-        <App />
-      </BrowserRouter>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook/">
+          <App />
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
