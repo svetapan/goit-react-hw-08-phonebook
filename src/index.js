f87959d6daa5829399cbc0a32706e58da4cd6446
Svetlana Panenko <svetabagira1986@gmail.com>
2023-06-21 21:51:45 +0300

    fixed Log Out

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-        {/* <BrowserRouter basename="/goit-react-hw-08-phonebook/"> */}
+        {/* <BrowserRouter> */}
+        <BrowserRouter basename="/goit-react-hw-08-phonebook/">
           <App />
         </BrowserRouter>
       </PersistGate>
